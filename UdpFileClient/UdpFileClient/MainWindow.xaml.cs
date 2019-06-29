89c15224f5c0b823b3fcd64b5e275241081b0541
RakhimovSerik <s_astana@mail.ru>
2019-06-29 19:16:31 +0600

    first commit

---
+++ UdpFileClient/UdpFileClient/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UdpFileClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Socket srvSock;
+        static string FileName;
+        static string FileFullName;
+        static int OneBlockSize;
+        static IPEndPoint ClientEndPoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbIpAdress.Text == "")
+            {
+                MessageBox.Show("Введите IP-адрес. Повторите ввод.");
+                return;
+            }
+
+            if (tbPort.Text == "")
+            {
+                MessageBox.Show("Введите порт. Повторите ввод.");
+                return;
+            }
+
+            try
+            {
+                OneBlockSize = int.Parse(tbOnBlockSize.Text);
+                if (OneBlockSize <= 0)
+                {
+                    MessageBox.Show("Введите число в OnBlockSize. Повторите ввод.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите число в OnBlockSize. Повторите ввод.");
+                return;
+            }
+
+            if (tbFileName.Text == "")
+            {
+                MessageBox.Show("Введите имя файла. Повторите ввод.");
+                return;
+            }
+
+            srvSock = new Socket(AddressFamily.InterNetwork,
+            SocketType.Dgram, ProtocolType.Udp);
+
+            ClientEndPoint = new IPEndPoint(IPAddress.Parse(tbIpAdress.Text),
+                                                      Convert.ToInt32(tbPort.Text));
+
+            ThreadPool.QueueUserWorkItem(ThreadRoutine);
+
+        }
+
+        static void ThreadRoutine(object obj)
+        {
+
+        FileStream outFile = new FileStream(FileFullName, FileMode.Open);
+
+        // количество посылок
+        int cntRecive = (int)outFile.Length / OneBlockSize;
+        // размер в байтах последней посылки
+        int Remainder = (int)outFile.Length % OneBlockSize;
+
+        // посылаем имя файла
+        byte[] data = Encoding.UTF8.GetBytes(FileName);
+        srvSock.SendTo(data, ClientEndPoint);
+
+            // посылаем количество посылок
+            int BlockCountToSend = cntRecive;
+            if (Remainder > 0) BlockCountToSend++;
+            data = BitConverter.GetBytes(BlockCountToSend);
+            srvSock.SendTo(data, ClientEndPoint);
+
+            // посылаем части файла
+            byte[] buf = new byte[OneBlockSize];
+            for (int i = 0; i<cntRecive; i++)
+            {
+                outFile.Read(buf, 0, OneBlockSize);
+                srvSock.SendTo(buf, 0, OneBlockSize, SocketFlags.None, ClientEndPoint);
+                Thread.Sleep(1000);  // без этой задержки большие файлы не пересылаются до конца
+            }
+            if (Remainder > 0)
+            {
+                outFile.Read(buf, 0, Remainder);
+                srvSock.SendTo(buf, 0, Remainder, SocketFlags.None, ClientEndPoint);
+            }
+            outFile.Flush();
+            outFile.Close();
+}
+
+        private void btClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All files (*.*)|*.*|Jpeg Files (*.jpg)|*.jpg";
+
+            if (ofd.ShowDialog() == true)
+            {
+                tbFileName.Text = ofd.FileName;
+                FileFullName = ofd.FileName;
+                FileName = ofd.SafeFileName;
+            }
+        }
+    }
+
+}
---
+++ UdpFileServer/UdpFileServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+// Сервер, принимающий по Udp имя файла, размер файла, содержимое файла
+namespace UdpFileServer
+{
+    class Program
+    {
+        static Socket srvSock;
+        static string ipServer = "0.0.0.0";
+        static int portServer = 12345;  // 0..65535
+        static EndPoint ClientEndPoint;
+        static void Main(string[] args)
+        {
+            srvSock = new Socket(AddressFamily.InterNetwork,
+                SocketType.Dgram, ProtocolType.Udp);
+            srvSock.Bind(new IPEndPoint(IPAddress.Parse(ipServer), portServer));
+
+            ClientEndPoint = new IPEndPoint(0, 0);
+
+            ThreadPool.QueueUserWorkItem(ThreadRoutine);
+         
+            Console.ReadLine();
+
+        }
+
+        static void ThreadRoutine(object obj)
+        {
+            // получить имя файла
+            byte[] buf = new byte[64 * 1024];  // рекомендуется для UDP
+            int recSize1 = srvSock.ReceiveFrom(buf, ref ClientEndPoint);
+            string FileName = Encoding.UTF8.GetString(buf, 0, recSize1);
+            Console.WriteLine("\nFile name = " + FileName);
+            FileStream outFile = new FileStream(FileName, FileMode.Create);
+
+            // получить кол-во посылок
+            srvSock.ReceiveFrom(buf, ref ClientEndPoint);
+            int cntRecive = BitConverter.ToInt32(buf, 0);
+            Console.WriteLine($"cntRecive = {cntRecive}");
+
+            // прием частей файла
+            int fullSize = 0;
+            for (int i = 0; i < cntRecive; i++)
+            {
+                int recSize = srvSock.ReceiveFrom(buf, ref ClientEndPoint);
+                if (recSize <= 0) break;
+                Console.WriteLine($"i = {i}, {recSize} bytes   \r");
+                outFile.Write(buf, 0, recSize);
+                fullSize += recSize;
+            }
+
+            Console.WriteLine($"\nFull size of file  = {fullSize}");
+            outFile.Flush(true);  // принудительный сброс данных на диск
+                                  //outFile.Seek()
+            outFile.Close();  // принудительное закрытие потоков
+                              //outFile.Dispose();
+        }
+    }
+}
